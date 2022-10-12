6bf0e28a31364cd17642c2ae94684ccd9bb424b1
MrPanda05 <Buttercup98Cool@protonmail.com>
2022-10-12 03:02:34 -0300

    game core finished

--- Le_Game.py
+++ Le_Game.py
@@ -1,4 +1,5 @@
 from random import *
+import json
  
 '''
     Gdd jogo copia do show do milhao
@@ -24,47 +25,172 @@ from random import *
 '''
  
 #Global variables
-numPoints = 0
-numPulos = 0
-numCincin = 0
-numUni = 0
-numPlat = 0
-playerName = ''
- 
+letters = ['a', 'b', 'c', 'd']
+specialWords = ['pulo', '50', 'plat', 'plateia', 'uni', 'universitarios', 'parar', 'skip', 'pular', '50|50', 'metade', 'stop', 'para', 'eliminar', 'elemina']
+stopWords = ['stop', 'parar', 'para']
+platWords = ['plat', 'plateia']
+cinWords = ['metade', '50', '50|50', 'eliminar', 'elemina']
+uniWords = ['uni', 'universitarios']
+skipWords = ['pular', 'pula', 'skip', 'pulo']
+
+totalJumps = 0
+total50 = 0
+totalUni = 0
+totalPlat = 0
+totalPoints = 0
+playerNick = ""
+
+
+player = {
+    "Quantidade_de_pontos": totalPoints,
+    "Pulos_usados" : totalJumps,
+    "50|50_usados": totalJumps,
+    "Universitarios_usados": totalUni,
+    "Plateias_usadas": totalUni,
+    "Nome_do_jogador": playerNick,
+}
+
+file = open('questions.json')
+
+data = json.load(file)
+
+#Leitura do json. data["questions"][n]
+#n eh para qual questao deve ser lida
+#print(data["questions"][0]["inco"][0])
  
 
- 
+#Selects the correct answer
+##Arg: n => the question number from json file
+### Return the correct letter and answers
+def ChooseCorrect(n):
+    #0:A| 1:B | 2:C | 3:D
+    correct = data["questions"][n]["correct"]
+    inco = data["questions"][n]["inco"]
+    inco.append(data["questions"][n]["correct"])
+    shuffle(inco)
+    num = 0
+    for i in inco:
+        if(i == correct):
+            print(f"Found correct answer {inco[num]} on index {num} letters {letters[num]}")
+            return letters[num], inco
+        num += 1
+
+#ChooseCorrect(0)
+    
+#Show questions test
+##Arg:answers => the list of answers
+def ShowQuestions(answers, how = 'default'):
+    num = 0
+    print(f"Pergunta numero {num + 1}")
+    if(how == 'default'):
+        for i in answers:
+            print(f"{letters[num]}---{i}\n")
+            num += 1
+    elif(how == 'uni'):
+        print(0)
+    
+
+    
+#Get answer for question
+##Arg: num => question number
+###Todo add special case in show question
+def GetAnswer(num):
+    correct, answers = ChooseCorrect(num)
+    ShowQuestions(answers)
+    isAnswering = True
+    inSpecialCase = False
+    while(isAnswering):
+        userInput = input("Digite sua resposta\n")
+        userInput.lower()
+        if(userInput in specialWords):
+            print("Especial")
+            inSpecialCase = True
+            while(inSpecialCase):
+                ShowQuestions(answers, detectCase(userInput))
+            isAnswering = False
+        elif(userInput in letters):
+            if(userInput == correct):
+                print("Correct")
+                player["Quantidade_de_pontos"] += 100
+                isAnswering = False
+            else:
+                print("Incorreto")
+                player["Quantidade_de_pontos"] -= 100
+                isAnswering = False
+        else:
+            print("Input invalido")
+
+    print(1)
+#Detect what special word it is
+def detectCase(word):
+    word.lower()
+    if(word in skipWords):
+        return 'skip'
+    if(word in uniWords):
+        return 'uni'
+    if(word in stopWords):
+        return 'stop'
+    if(word in platWords):
+        return 'plat'
+    if(word in cinWords):
+        return 'cin'
+    
 #Create a random array with n numbers in it
-def RandomQuestions(n):
+##Arg: n => lenght of array
+def ArrayGenerator(n):
     arr = []
     #Loops and adds i to array
-    for i in range(1,n + 1):
+    for i in range(0,n):
         arr.append(i)
-    #print(arr)
     return arr
  
-#Embaralha as questoes
-def ShuffleQuestions():
-    newArr = RandomQuestions(15)
-    shuffle(newArr)
-    #print(newArr)
-    return newArr
+#Shuffles array
+##Arg: arr => array to be shuffle
+def ShuffleArray(arr):
+    shuffle(arr)
+    return arr
  
+
 #Make a special question case
 #if num is divisible by 5
 def SpecialQuestion():
-    newArr = ShuffleQuestions()
+    newArr = ShuffleArray()
     oldArr = []
     for i in range(0, len(newArr)):
         if(newArr[i] % 5 == 0 and newArr[i] != 0):
             oldArr.insert(i, newArr[i])
     return oldArr
- 
- 
+
+
+#Creates json file for user score
+def SaveResults():
+    with open('data.json', 'w') as outfile:
+        json.dump(player, outfile, indent=4)
+
  
 def Main():
-    #O jogo completo estara aki
-    a = 'oi'
+    totalQuestion = len(data["questions"])
+    gameIsPlaying = False
+    listOfQuestions = ArrayGenerator(totalQuestion)
+    ShuffleArray(listOfQuestions)
+    gameIsPlaying = True
+    playerNick = input("Digite seu nome!\n")
+    player["Nome_do_jogador"] = playerNick
+    if(playerNick == ""):
+        player["Nome_do_jogador"] = "Gamer"
+    num = 0
+    rounds = 0
+    while(gameIsPlaying):
+        GetAnswer(listOfQuestions[num])
+        rounds += 1
+        num += 1
+        print(player["Quantidade_de_pontos"])
+        if(rounds >= totalQuestion):
+            break
+    SaveResults()
+   
+
+#Main()
  
  
 # #List de numeros de questoes no jogo
